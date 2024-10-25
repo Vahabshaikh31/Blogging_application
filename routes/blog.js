6ecf65971a623b74e9bd156319f9f370720c7439
abdulvahab shaikh <abdulvahab@vahab.vahab>
2024-10-25 17:32:29 +0530

    first commit

--- routes/blog.js
+++ routes/blog.js
@@ -30,7 +30,7 @@ router.post("/", upload.single("coverImage"), async (req, res) => {
       createdBy: req.user._id,
       coverImageURL: `/uploads/${req.file.filename}`,
     });
-
+    
     return res.redirect(`/blog/${blog._id}`);
   } catch (error) {
     console.error(error);
